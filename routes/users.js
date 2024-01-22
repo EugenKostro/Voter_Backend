2fb629b0327309c8867e95573dffc9653e03f36d
Eugen101 <55134543+EugenKostro@users.noreply.github.com>
2024-01-22 18:32:31 +0100

    commit
    commit

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,13 @@
 const express = require('express')
 const router = express.Router();
-const User = require('../models/User');
+const { hashSync, compareSync } = require('bycriptjs');
+const { v4: uuidv4 } = requre('uuid');
 
-router.post('/register', async function(req, res, next){
-    try{
-        const user = new User(req.body);
-        await user.save();
-        res.status(201).send({message: 'User registered successfully!'});
-    }
-    catch(error){
-        res.status(400).send({error: error.message});
-    }
-});
+router.post('/register', async function(req, res){
+    const{ username, password } = req.body;
 
-module.exports = router;
+    const existingUser = await Db.collection('users').findOne({ username });
+    if(existingUser){
+        return res.status(400).
+    }
+})
